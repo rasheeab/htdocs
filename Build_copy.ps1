cab9d3914cbc8d7e5a33594550e4c1bae6f82337
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 16:59:24 -0400

    commit all to main

---
+++ Build_copy.ps1
@@ -0,0 +1,2 @@
+###Script to copy the build to Dev env for testing
+Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.${BUILD_NUMBER}\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
