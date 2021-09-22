c84fd978d581c099768f6999888049d237cc135e
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 22:19:36 -0400

    Update Build_copy.ps1

--- Build_copy.ps1
+++ Build_copy.ps1
@@ -2,11 +2,11 @@
 $build_number=$args[0]
 
 Write-Output "creating a build tag#########$build_number###"
-Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)" -Recurse
+Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\Bank_statement_upload\\BS_V_1.$($build_number)" -Recurse
 
 
 Write-Output "installing#########$build_number### to integration"
-Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
+Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\Bank_statement_upload\\BS_V_1.$($build_number)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
 
 
 
