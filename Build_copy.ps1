9c21bb82a5c8458925c13c39d9a5d244807b57de
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 21:52:47 -0400

    update

--- Build_copy.ps1
+++ Build_copy.ps1
@@ -1,10 +1,9 @@
 ###Script to copy the build to Dev env for testing
 $build_number=$args[0]
 
-Write-Output $build_number
+Write-Output "$build_number"
 Write-Output "Copying build to version##################"
-Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)" -Recurse
-Write-Output "installing to Dev##################"
+
 Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
 
 
