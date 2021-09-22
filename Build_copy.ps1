7e0bef51ad13c845f6619827422c53e0d9ec76f5
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 22:00:02 -0400

    up

--- Build_copy.ps1
+++ Build_copy.ps1
@@ -1,18 +1,20 @@
 ###Script to copy the build to Dev env for testing
 $build_number=$args[0]
 $build_n=$args[1]
+$build_bak=$args[2]
 
 Write-Output $build_number
-Write-Output "Copying build to version#########$build_number#########"
-
-Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
-
-
-
 Write-Output $build_n
-Write-Output "Copying build to version#########$build_n#########"
+Write-Output $build_bak
 
-Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_n)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
+Write-Output "one#########$build_number#########"
+Write-Output "two#########$build_n#########"
+Write-Output "three#########$build_bak###"
+#Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_n)" -Recurse
+
+
+
+#Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_n)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
 
 
 
