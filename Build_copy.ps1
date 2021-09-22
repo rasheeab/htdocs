364d29b21a5d1fe65f2d0fee0640774d4624d2a9
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 22:28:25 -0400

    Update Build_copy.ps1

--- Build_copy.ps1
+++ Build_copy.ps1
@@ -1,6 +1,8 @@
 ###Script to copy the build to Dev env for testing
 $build_number=$args[0]
 
+md -path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\Bank_statement_upload\\BS_V_1.$($build_number)"
+
 Write-Output "creating a build tag#########$build_number###"
 Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\Bank_statement_upload\\BS_V_1.$($build_number)" -Recurse
 
