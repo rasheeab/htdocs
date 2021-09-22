debbc0305e80973e6400b7a0f466b350047038ea
rasheed.fp@gmail.com <rasheed.fp@gmail.com>
2021-09-21 22:14:03 -0400

    Update Build_copy.ps1

--- Build_copy.ps1
+++ Build_copy.ps1
@@ -1,20 +1,12 @@
 ###Script to copy the build to Dev env for testing
 $build_number=$args[0]
-$build_n=$args[1]
-$build_bak=$args[2]
 
-Write-Output $build_number
-Write-Output $build_n
-Write-Output $build_bak
-
-Write-Output "one#########$build_number#########"
-Write-Output "two#########$build_n#########"
-Write-Output "three#########$build_bak###"
-#Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_n)" -Recurse
+Write-Output "creating a build tag#########$build_number###"
+Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Bank_statement_upload\\*" -Destination "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)" -Recurse
 
 
-
-#Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_n)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
+Write-Output "installing#########$build_number### to integration"
+Copy-Item -Path "C:\\Users\\rashe\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\build\\V_1.$($build_number)\\*" -Destination "C:\\Dev\\Web\\Apache24\\htdocs" -Recurse
 
 
 
